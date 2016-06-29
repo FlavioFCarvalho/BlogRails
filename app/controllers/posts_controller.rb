9774a3fb75dbeec0af69f82fdd5d6588fcd35da8
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-06-29 16:15:11 -0300

    Editando o Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,20 @@ class PostsController < ApplicationController
 
     def show
         @post = Post.find(params[:id])
-    end    
+    end 
+
+    def edit
+        @post = Post.find(params[:id])       
+    end  
+
+    def update
+        @post = Post.find(params[:id]) 
+        if @post.update(params[:post].permit(:title, :body))
+            redirect_to @post
+        else
+            render 'edit'
+        end        
+    end 
 
     private 
     	def post_params
